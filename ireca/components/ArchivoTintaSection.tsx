415fa80f231cc28bb05c51520b0c655ba0508e7a
LuisMelladoDiaz <melladodiazluis@gmail.com>
2026-06-20 21:12:52 +0200

    chore: renaming and reordering resources

--- ireca/components/ArchivoTintaSection.tsx
+++ ireca/components/ArchivoTintaSection.tsx
@@ -17,8 +17,8 @@ export default function ArchivoTintaSection() {
 
       <SerieLlaves />
       <SerieSelvatico />
-
-      {<SerieOtherworldly />}
+      <SerieOtherworldly />
+      
     </div>
   );
 }
